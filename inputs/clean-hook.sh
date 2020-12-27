9d43ef05a9c4f350a59bc8b0773140f8e5e40781
ClementToche <3122713+ClementToche@users.noreply.github.com>
2020-12-27 19:41:28 +0100

    Fix API Key variable name

--- inputs/clean-hook.sh
+++ inputs/clean-hook.sh
@@ -5,4 +5,4 @@ echo "Cleaning for $CERTBOT_DOMAIN"
 root_domain=$(echo -en $CERTBOT_DOMAIN | rev | cut -d . -f1,2 | rev)
 rec_name=$(echo _acme-challenge.$CERTBOT_DOMAIN | sed 's/.'$root_domain'//g')
 
-curl -s -D- -X DELETE -H "Content-Type: application/json" -H "X-Api-Key: $GANDI_APIKEY" https://dns.api.gandi.net/api/v5/domains/$root_domain/records/$rec_name/TXT > /dev/null
+curl -s -D- -X DELETE -H "Content-Type: application/json" -H "X-Api-Key: $PCS_GANDI_APIKEY" https://dns.api.gandi.net/api/v5/domains/$root_domain/records/$rec_name/TXT > /dev/null
